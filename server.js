93b08af597df5755dff6828d9a5b0d8d063e13c6
Alessandro Loiacono <alessandroloiacono1994@gmail.com>
2022-03-01 17:34:51 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000); //porta HTTP 3000 in ascolto
+
+app.get('/', function(req,res) {
+    res.send('Ciao Mondo, sono una APP DI PROVA');
+});
+
+app.listen(app.get('port'),function() {
+    console.log('Esempio di web app che gira sulla porta : ' + app.get('port'));
+});
+
