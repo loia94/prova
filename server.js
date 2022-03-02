0cc4c82a4a2761f82b99f0061c2fbb57a921880d
Alessandro Loiacono <alessandroloiacono1994@gmail.com>
2022-03-02 09:47:52 +0100

    first deploy

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-var express = require('express');
+var express = require('express'); //utilizziamo la tecnologia express per node.js (una libreria)
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000); //porta HTTP 3000 in ascolto
 
 app.get('/', function(req,res) {
-    res.send('Ciao Mondo, sono una APP DI PROVA');
+    res.send('Ciao Mondo, sono una APP DI PROVA - first deploy');
 });
 
 app.listen(app.get('port'),function() {
     console.log('Esempio di web app che gira sulla porta : ' + app.get('port'));
 });
 
+//descrizione del progetto in package.json
